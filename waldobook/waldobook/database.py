8ff501ca9c25d41b33d25c38e2cd3046899efb58
Max Katz-Christy <maxkatzchristy@gmail.com>
2023-01-27 16:46:29 -0500

    postgresql not postgres

--- waldobook/waldobook/database.py
+++ waldobook/waldobook/database.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL") or ""
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    'postgresql://' + SQLALCHEMY_DATABASE_URL.split('://')[1], connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
