29a3d86186802628891daf08dbe39049decfcd7d
Max Katz-Christy <maxkatzchristy@gmail.com>
2023-01-27 16:58:54 -0500

    remove psql args

--- waldobook/waldobook/database.py
+++ waldobook/waldobook/database.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL") or ""
 
 engine = create_engine(
-    'postgresql://' + SQLALCHEMY_DATABASE_URL.split('://')[1], connect_args={"check_same_thread": False}
+    'postgresql://' + SQLALCHEMY_DATABASE_URL.split('://')[1]
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
