538b8dc92d7ad5db9a0a2fdc17b54dd12b7a1cf3
MarkRazanau <mark.razanau@gmail.com>
2023-02-01 11:47:19 -0500

    messing with login screen

--- treasuremit/src/pages/index.tsx
+++ treasuremit/src/pages/index.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
-import background from "../../public/mittreasure.png"
+import background from "../../public/background_noicons.png"
+import backIcons from "../../public/background_icons.png"
 import mascot from "../../public/beaver.png"
+import mascotEyes from "../../public/beaver_eyes.png"
+import mascotNose from "../../public/beaver_nose.png"
 import mascotArms from "../../public/beaver_loading.png"
 import LogNavBar from "../../components/LogNavBar"
 
@@ -16,6 +19,9 @@ export default function Main(){
         <div className="Login-page">
             <LogNavBar/>
             <div className="Login-container-shadow"></div>
+            
+            <Image className="Login-backimg" src={background} alt="MIT Treasure Map"/>
+            <Image className="Login-backimg" src={backIcons} alt="MIT Treasure Map"/>
             <div className="Login-beaver-wrapper">
                 <Image className="Login-beaver" src={mascot} alt="TreasureMIT Beaver"/>
                 <Image className="Login-loading" src={mascotArms} style={{visibility: showLoad ? "visible" : "hidden"}} alt="TreasureMIT Beaver"/>
@@ -23,14 +29,13 @@ export default function Main(){
             <div className="Login-container">
                 <div className="Login-welcome">Welcome to TreasureMIT!</div>
                 <div className="Login-info">
-                    Start your Geocache adventure right here on MIT campus and start looking for hidden treasures and caches!
+                    Start your Treasure Hunting adventure right here on MIT campus and start looking for goodies hidden away by your peers!
                 </div>
                 {/* <a href="https://oidc.mit.edu/authorize?client_id=baa96962-f4a6-4451-9c04-1fcf05c46c12&response_type=code&scope=openid phone email address profile offline_access&redirect_uri=http://localhost:3000/redirect" className='Login-submit' onClick={toggleLoad}><div className="Login-submit-text">Continue With MIT ID</div></a> */}
                 <a href="https://oidc.mit.edu/authorize?client_id=baa96962-f4a6-4451-9c04-1fcf05c46c12&response_type=code&scope=openid phone email address profile offline_access&redirect_uri=https://treasuremit.herokuapp.com/redirect" className='Login-submit' onClick={toggleLoad}><div className="Login-submit-text">Continue With MIT ID</div></a>
                 {/* <button className="Login-submit">Start Hunting!</button> */}
             </div>
             <div className="Login-backimg-wrapper">
-            <Image className="Login-backimg" src={background} alt="MIT Treasure Map"/>
             </div>
         </div>
     )
